34c442b64ae8e6052c03a8f1257ee660e1cdcfb6
Guilherme Melo <guilherme_liver@hotmail.com>
2019-02-14 20:46:56 -0300

    Delete path

--- server/server.js
+++ server/server.js
@@ -51,6 +51,20 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(req.params.id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }, (err) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`App is listening on ${port}!`);
 });
