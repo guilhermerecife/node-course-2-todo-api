5b6c34f6a9c7e3ed07c923fada330ad0fd64901e
Guilherme Melo <guilherme_liver@hotmail.com>
2019-02-14 22:11:22 -0300

    Bug

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
+import { ENODEV } from "constants";
+
 var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(end === 'test') {
+}else if(ENODEV === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
